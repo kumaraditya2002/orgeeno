bdee1e6c342b6a161c4450f768eff4f570b5015c
Aditya Kumar <adityakumar9006@gmail.com>
2020-12-27 14:33:17 +0530

    aise hi

--- public/js/script.js
+++ public/js/script.js
@@ -2,26 +2,26 @@
    let scrolled;
    window.addEventListener('scroll',()=>{
            scrolled  = window.scrollY;
-        //    console.log(scrolled);
+          //  console.log(scrolled);
+        });
+        const counter = document.querySelectorAll(".counter");
+        counter.forEach(counter => {
+            const updateCount = () => {
+                const target = +counter.getAttribute('data-target');
+                const count = +counter.innerText;
+                const inc = 1;   
+                if(count < target  && scrolled >=900){
+                   counter.innerText = count + inc;
+                   setTimeout(updateCount, 3);
+                }
+                else{
+                    count.innerText = target;
+                    setTimeout(updateCount, 3);
+                }
+            }
+            updateCount();
         });
    
-   const counter = document.querySelectorAll(".counter");
-   counter.forEach(counter => {
-       const updateCount = () => {
-           const target = +counter.getAttribute('data-target');
-           const count = +counter.innerText;
-           const inc = 1;   
-           if(count < target  && scrolled >=240){
-              counter.innerText = count + inc;
-              setTimeout(updateCount, 3);
-           }
-           else{
-               count.innerText = target;
-               setTimeout(updateCount, 3);
-           }
-       }
-       updateCount();
-   });
 
    // Scroll to top button
 $(window).scroll(function () {
