cb3a0bd480e915663b42f1df1b97aa191141e1ac
Aditya Kumar <adityakumar9006@gmail.com>
2020-12-27 11:48:05 +0530

    animation counter

--- public/js/script.js
+++ public/js/script.js
@@ -1,6 +1,24 @@
-jQuery(document).ready(function ($) {
-    $('.counter').counterUp({
-        delay: 20,
-        time: 2200
-    });
-});
+//scroll animation
+   let scrolled;
+   window.addEventListener('scroll',()=>{
+           scrolled  = window.scrollY;
+        //    console.log(scrolled);
+        });
+   
+   const counter = document.querySelectorAll(".counter");
+   counter.forEach(counter => {
+       const updateCount = () => {
+           const target = +counter.getAttribute('data-target');
+           const count = +counter.innerText;
+           const inc = 1;
+           if(count < target  && scrolled >=240){
+              counter.innerText = count + inc;
+              setTimeout(updateCount, 3);
+           }
+           else{
+               count.innerText = target;
+               setTimeout(updateCount, 3);
+           }
+       }
+       updateCount();
+   });
